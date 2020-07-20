78d93bf56231a9e8e1c62f88005a83b54fd338c3
iljoo chae <9662964@gmail.com>
2020-07-20 11:46:01 -0400

    finished lectur 3

---
+++ DDEContact/Helper + Extension/UIColor.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor.swift
+//  DDEContact
+//
+//  Created by iljoo Chae on 7/20/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let someColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+    static let tealColor = UIColor(red: 48/255, green: 164/255, blue: 182/255, alpha: 1)
+    static let lightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+    static let lightBlue = UIColor(red: 218/255, green: 235/255, blue: 243/255, alpha: 1)
+    
+}
--- DDEContact/ViewController.swift
+++ DDEContact/ViewController.swift
@@ -19,16 +19,25 @@ class ViewController: UITableViewController {
         tableView.backgroundColor = UIColor(red: 9/255, green: 45/255, blue: 64/255, alpha: 1)
         //able to remove tableView seperator(line)
         //tableView.separatorStyle = .none
+        
+        //just show tableview seperator as it has been assigned,and rest of the space show no seperators.
+        tableView.tableFooterView = UIView()
+        
+        //Add plus btn
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "plus"), style: .plain, target: self, action: #selector(handleAddCompany))
         
+        //Assign cell ID
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        //seperator color
+        tableView.separatorColor = .white
     }
     
     func setupNavigationStyle() {
-        let lightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        navigationController?.navigationBar.backgroundColor = lightRed
+        navigationController?.navigationBar.backgroundColor = UIColor.lightRed
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.barTintColor = .red
         
@@ -39,11 +48,26 @@ class ViewController: UITableViewController {
     @objc func handleAddCompany() {
         print("Adding compnay")
     }
+
+    //header
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .lightBlue
+        return view
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
         
-        
+        cell.textLabel?.text = "The Company Name"
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        cell.backgroundColor = UIColor.tealColor
         return cell
     }
     
